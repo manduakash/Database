066553515531ef022029a4cbb11259427f146d28
manduakash <50767686+manduakash@users.noreply.github.com>
2022-08-24 12:09:59 +0530

    Update emp_dept_Tabele.sql

--- emp_dept_Tabele.sql
+++ emp_dept_Tabele.sql
@@ -34,5 +34,26 @@ select * from emp;
 #displaying department table
 select * from dept;
 
+#altering 
+alter table dept rename column salary to basic_salary;
+alter table dept rename column dept_name to designation;
 
+#updating 
+update dept set designation='traniee' where basic_salary='100000';
+update dept set designation='traniee' where designation='Jr. engineer';
+
+#deleting using delete command
+delete from emp where emp_name='akash';
+
+#deleting using truncate command
+truncate table emp;
+
+#deleting using drop command
+drop table emp;
+
+#displaying employee table
+select * from emp;
+
+#displaying department table
+select * from dept;
 
